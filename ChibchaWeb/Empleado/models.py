bcbfca6e19906677e31f8da45975bbfe39741ec6
Code0blivion <bautistaluis1002@gmail.com>
2023-11-30 00:38:42 -0500

    Arreglo tickets

--- ChibchaWeb/Empleado/models.py
+++ ChibchaWeb/Empleado/models.py
@@ -8,7 +8,7 @@ class Empleado(models.Model):
     nombreEmpleado = models.CharField(verbose_name="nombre", max_length=20)
     fechaNacimientoEmpleado = models.DateField(verbose_name="Fecha de Nacimiento", blank=True, null=True)
     cargoEmpleado = models.CharField(verbose_name="cargoEmpleado", blank=False,max_length=15 )
-    inicioContrato = models.DateField(verbose_name="Fecha de Nacimiento", blank=True, null=True)
+    inicioContrato = models.DateField(verbose_name="Inicio de contrato", blank=True, null=True)
     telefonoEmpleado = models.CharField(blank=False, max_length=20)
     emailEmpleado = models.CharField(verbose_name="emailEmpleado", max_length=30)
     
